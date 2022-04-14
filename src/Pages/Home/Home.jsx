e8e924b53f5523763854d1a8922126839be24385
ashikmahmud <ashikmamud187@gmail.com>
2022-04-15 01:27:18 +0600

    added todo dashboard of ToDos Application

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import CreateTodo from "../Todos/CreateTodo/CreateTodo";
-
+import Todo from "../Todos/Todo/Todo";
 const Home = () => {
   return (
     <HomeContainer>
       <CreateTodo />
+      <Todo />
     </HomeContainer>
   );
 };
---
+++ src/Pages/Todos/Todo/Todo.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BiCheckDouble } from "react-icons/bi";
+import { BsTrash } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
+import styled from "styled-components";
+const Todo = () => {
+  return (
+    <TodoContainer>
+      <div className="container">
+        <h1>Todo</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>ToDo</th>
+              <th style={{ width: "100px" }}>Action</th>
+              <th style={{ width: "100px" }}>Edit</th>
+              <th style={{ width: "100px" }}>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Get out outside</td>
+              <td>
+                <span className="cursor-pointer check">
+                  <BiCheckDouble />
+                </span>
+              </td>
+              <td>
+                <span className="cursor-pointer">
+                  <FiEdit />
+                </span>
+              </td>
+              <td>
+                <span className="cursor-pointer">
+                  <BsTrash />
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </TodoContainer>
+  );
+};
+
+const TodoContainer = styled.div`
+  position: relative;
+  color: var(--accent-color);
+  .check {
+    font-size: 2rem;
+    color: #07e530ec;
+  }
+`;
+
+export default Todo;
