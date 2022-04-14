32edfa28a4db3171c8251938c270538a86311a16
ashikmahmud <ashikmamud187@gmail.com>
2022-04-15 00:07:04 +0600

    Initialization react todos application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1>Hello World</h1>
+     <button className="btn">Button Primary</button>
+     <button className="btn-outline">Button Outlined</button>
+     <table>
+         <thead>
+            <tr>
+            <th>SL No</th>
+             <th>Name</th>
+             <th>App</th>
+            </tr>
+         </thead>
+         <tbody>
+             <tr>
+                 <td>1</td>
+                 <td>Ashik</td>
+                 <td>Facebook</td>
+             </tr>
+         </tbody>
+     </table>
+    </>
   );
 }
 
